793eab2ef4aecb703e30f9e2a5b5efd95e704d5e
tmeits <tmeits@gmail.com>
2011-07-26 17:24:46 +0800

    add project.clj

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject four-clojure "1.0"
+  :description "Four-Clojure is a resource to help fledgling clojurians learn the language through interactive problems."
+  :dependencies [[org.clojure/clojure "1.2.1"]])
