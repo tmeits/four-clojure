19c352cf713fe382478f03e4c6728f2722ce9097
tmeits <tmeits@gmail.com>
2011-07-26 19:57:29 +0800

    add test s6-s11

--- show50.clj
+++ show50.clj
@@ -41,6 +41,34 @@
   (is (= '(1 2 3 4) (conj '(2 3 4) 1)))
   (is (= '(1 2 3 4) (conj '(3 4) 2 1))))         ; [coll x & xs]
 
+(deftest s6 "Intro to Vectors"
+  ;; Vectors can be constructed several ways. You can compare them with lists.
+  (is (= [:a :b :c]
+         (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))))
+
+(deftest s7 "Vectors: conj"
+  ;; When operating on a Vector, the conj function will return a new vector with one or more items "added" to the end.
+  (is (= [1 2 3 4] (conj [1 2 3] 4)))
+  (is (= [1 2 3 4] (conj [1 2] 3 4))))
+
+(deftest s8 "Intro to Sets"
+  ;; Sets are collections of unique values.
+  (is (= #{:a :b :c :d} (set '(:a :a :b :c :c :c :c :d :d))))
+  (is (= #{:a :c :b :d} (clojure.set/union #{:a :b :c} #{:b :c :d}))))
+
+(deftest s9 "Sets: conj"
+  ;; When operating on a set, the conj function returns a new set with one or more keys "added".
+  (is (= #{1 2 3 4} (conj #{1 4 3} 2))))
+
+(deftest s10 "Intro to Maps"
+  ;; Maps store key-value pairs. Both maps and keywords can be used as lookup functions. Commas can be used to make maps more readable, but they are not required.
+  (is (= 20 ((hash-map :a 10, :b 20, :c 30) :b)))
+  (is (= 20 (:b {:a 10, :b 20, :c 30}))))
+
+(deftest s11 "Maps: conj"
+  ;; When operating on a map, the conj function returns a new map with one or more key-value pairs "added".
+  (is (= {:a 1, :b 2, :c 3} (conj {:a 1} {:b 2} [:c 3])))
+  (is (= {:a 1, :b 2, :c 3} (conj {:a 1} [:b 2] [:c 3]))))
 (run-tests)
 
 ;; 21: Write a function which returns the Nth element from a sequence.
